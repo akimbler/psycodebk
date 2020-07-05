f791bcce68ea45b9238f6e375ba1a2a18c60231b
akimbler <akimb009@fiu.edu>
2020-07-05 15:59:07 -0400

    Fixes

--- psycodebk/viz/viz.py
+++ psycodebk/viz/viz.py
@@ -137,7 +137,7 @@ def plot_interval(variable: dict, dataset: pd.DataFrame) -> str:
         dataset[variable['name']].replace(
             to_replace=variable['naValues'],
             value=np.nan).to_numpy(),
-        color='#ff6e9c98')
+        color='#ff6e9c98', kde=False)
     plt.savefig(image, format='png', dpi=144, bbox_inches='tight')
     image_data = base64.encodebytes(
         image.getvalue()).decode('utf-8').strip('\n')
